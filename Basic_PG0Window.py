9390ed34858aec7ef0fb1889fff7a5a54cfb75e6
YuvDeeCode <yuvan.deepak20@gmail.com>
2026-01-14 18:54:38 +0000

    Update Basic_PG0Window.py

--- Basic_PG0Window.py
+++ Basic_PG0Window.py
@@ -1,7 +1,8 @@
-import pgzero
+import pgzrun
 WIDTH = 600
 HEIGHT = 600
 def draw():
     screen.clear()
     screen.draw.text("Welcome to pgzero!",center=(WIDTH/2,HEIGHT/2),fontsize=40)
 pgzrun.go()
+
