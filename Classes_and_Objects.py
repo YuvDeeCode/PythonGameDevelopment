c1f43f3979849381ee1b5e9e998ea407411a9858
YuvDeeCode <yuvan.deepak20@gmail.com>
2026-06-10 18:49:26 +0100

    Add files via upload

--- Classes_and_Objects.py
+++ Classes_and_Objects.py
@@ -6,3 +6,17 @@ class car:
         print(self.brand,"is driving")
 car1=car("Toyota")
 car1.drive()
+
+class student:
+    def __init__(self,name,grade,age):
+        self.name = name
+        self.grade = grade
+        self.age = age
+    def info(self):
+        print("Student name: "+str(self.name))
+        print("Student grade: "+str(self.grade))
+        print("Student age: "+str(self.age))
+student1=student("Yuv",9,13)
+student2=student("Mark",8,12)
+student1.info()
+student2.info()
